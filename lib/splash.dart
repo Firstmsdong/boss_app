d71aa71e10a785c504730f477908cf85c93a0c68
XiaoMaGe <first_msdong@163.com>
2021-03-01 17:43:06 +0800

    添加启动页功能编写

---
+++ lib/splash.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+// import 'package:flutter_boss/home.dart';
+
+class SplashPage extends StatefulWidget {
+
+  @override
+  SplashState createState() => new SplashState();
+}
+
+class SplashState extends State<SplashPage> {
+
+  Timer _t;
+
+  @override
+  void initState(){
+    super.initState();
+    _t = new Timer(const Duration(milliseconds: 15000), (){
+
+      // try{
+      //   Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context)=>new BossApp()),(Route route) => route == null);
+      // }catch(e){
+      //
+      // }
+
+    });
+  }
+
+  @override
+  void dispose(){
+    _t.cancel();
+    super.dispose();
+  }
+  //布局启动页面
+  @override
+  Widget build(BuildContext context){
+    return new Material(
+      color: new Color.fromARGB(255, 0, 215, 198),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 150),
+        child: Column(
+          children: [
+            Text("BOSS直聘",style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.w400),),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
