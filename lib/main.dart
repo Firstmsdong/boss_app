27704b519d411ed6a67cb131782f079d29bba60b
XiaoMaGe <first_msdong@163.com>
2021-03-01 16:52:23 +0800

    修改了一个数字

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 9000;
 
   void _incrementCounter() {
     setState(() {
